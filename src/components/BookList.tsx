c1981641dd5ef71d39782bf8071456288d24a1d0
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2023-07-04 01:11:54 +0500

    add key in book list

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -29,7 +29,7 @@ export const BookList = (): ReactNode => {
       {!books?.length
         ? showMessage("No Books found!")
         : books.map((book) => (
-            <li className="flex justify-between gap-x-6 py-5">
+            <li key={book.author} className="flex justify-between gap-x-6 py-5">
               <div className="flex gap-x-4">
                 <img
                   className="h-12 w-12 flex-none rounded-full bg-gray-50"
